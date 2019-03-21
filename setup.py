3ea52e1ab6a54ffc9d61da9d4d436d28098dc7ce
Achille Verheye <achille.verheye@gmail.com>
2019-03-21 12:04:03 -0700

    Fix bug with numpy quaternion installation (#6)
    * Solve error on install on systems that don't have numpy yet
    
    * Remove numpy uninstall and install from travis
    
    * Re-add uninstallation of default numpy

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
+
+# hack for https://github.com/moble/quaternion/issues/99
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 setup(name='dual_quaternions_ros',
       version='0.1',
@@ -13,6 +26,8 @@ setup(name='dual_quaternions_ros',
       author_email='achille.verheye@gmail.com',
       license='MIT',
       packages=['dual_quaternions_ros'],
+      cmdclass={'build_ext': build_ext},
+      setup_requires=['numpy'],
       install_requires=['numpy', 'numpy-quaternion', 'numba'],
       zip_safe=False,
       test_suite='nose.collector',
