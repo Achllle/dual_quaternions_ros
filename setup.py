f40efb5aab3ee0945e22dc783ae02a9f246e1475
Achille <achille.verheye@gmail.com>
2019-06-26 09:33:21 -0700

    Revert "Fix bug with numpy quaternion installation (#6)"
    This reverts commit 3ea52e1ab6a54ffc9d61da9d4d436d28098dc7ce.

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
 from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
-
 
 def readme():
     with open('README.rst') as f:
         return f.read()
-
-
-# hack for https://github.com/moble/quaternion/issues/99
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
 
 setup(name='dual_quaternions_ros',
       version='0.1',
@@ -26,8 +13,6 @@ setup(name='dual_quaternions_ros',
       author_email='achille.verheye@gmail.com',
       license='MIT',
       packages=['dual_quaternions_ros'],
-      cmdclass={'build_ext': build_ext},
-      setup_requires=['numpy'],
       install_requires=['numpy', 'scipy', 'numpy-quaternion', 'numba'],
       zip_safe=False,
       test_suite='nose.collector',
